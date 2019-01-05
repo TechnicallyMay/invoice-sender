10d50123207545dd9c0a136b57dbb0f078215bc0
TechnicallyMay <masonwells01@gmail.com>
2019-01-05 10:13:47 -0700

    Updated main.py

--- main/main.py
+++ main/main.py
@@ -1,26 +1,8 @@
-import pandas as pd
-from customer import Customer
+import customer
+from invoice_email import InvoiceEmail
+from owner import Owner
 
 
-file_name = "../data/customers.xlsx"
-file = pd.read_excel(file_name)
-num_of_customers = file.shape[0]
-data = file.to_dict()
-
-customers = []
-
-
-def read_customer(num):
-    customer_info = []
-    for key, values in data.items():
-        customer_info.append(values[i])
-    return Customer(customer_info)
-
-
-for i in range(num_of_customers):
-    customers.append(read_customer(i))
-    
-for customer in customers:
-    for key, value in vars(customer).items():
-        print(key, value)
-    print()
+owner = Owner()
+email = InvoiceEmail(owner)
+print(email.find_key_words())
