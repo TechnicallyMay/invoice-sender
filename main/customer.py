cba640ddeb1c4d731be6caf46179aa9a435bca08
TechnicallyMay <masonwells01@gmail.com>
2019-01-05 10:13:23 -0700

    Moved customer list to file.

--- main/customer.py
+++ main/customer.py
@@ -1,4 +1,12 @@
 import math
+import pandas as pd
+
+
+customers = []
+file_name = "../data/customers.xlsx"
+file = pd.read_excel(file_name)
+num_of_customers = file.shape[0]
+data = file.to_dict()
 
 
 class Customer():
@@ -6,6 +14,7 @@ class Customer():
     def __init__(self, info):
         self.first_name = info[0]
         self.last_name = info[1]
+        self.full_name = self.first_name + " " + self.last_name
         self.email = info[2]
         self.num_of_students = info[3]
         if math.isnan(info[4]):
@@ -17,3 +26,14 @@ class Customer():
             self.fees = None
         else:
             self.fees = info[6]
+
+
+def read_customer(num):
+    customer_info = []
+    for key, values in data.items():
+        customer_info.append(values[i])
+    return Customer(customer_info)
+
+
+for i in range(num_of_customers):
+    customers.append(read_customer(i))
