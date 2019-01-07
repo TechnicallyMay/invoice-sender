5c361a88f1e0a05bc95105ea7c9b3bbbf05c18ac
TechnicallyMay <masonwells01@gmail.com>
2019-01-06 19:31:10 -0700

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,8 +1,20 @@
-import customer
+import people
+import pandas as pd
 from invoice_email import InvoiceEmail
-from owner import Owner
 
 
-owner = Owner()
-email = InvoiceEmail(owner)
-print(email.find_key_words())
+owner = people.Owner()
+customers = []
+customer_file = pd.read_excel("../data/customers.xlsx")
+num_of_customers = customer_file.shape[0]
+for i in range(num_of_customers):
+    customers.append(people.Customer(i))
+
+email = InvoiceEmail(owner, customers[0])
+email.replace_key_words()
+# print(email.body)
+
+for customer in customers:
+    for key, value in vars(customer).items():
+        print(key, value)
+    print()
