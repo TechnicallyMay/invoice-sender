ac31a1f5efd506f530a8da0b5d6090b5c4b06f9e
TechnicallyMay <masonwells01@gmail.com>
2019-01-08 13:47:05 -0700

    Updated main.py

--- main/main.py
+++ main/main.py
@@ -13,25 +13,27 @@ num_of_customers = customer_file.shape[0]
 for i in range(num_of_customers):
     customers.append(people.Customer(i))
 owner.data["Due Date"] = translate.translate(owner.data["Due Date"], customers[0].data, owner.data)
-
-emails = []
-for customer in customers:
-    emails.append(InvoiceEmail(owner, customer))
-
-print("Sending following emails: ")
-for email in emails:
-    print(email.customer.data["Name"])
-
-choice = input("Continue? (Y/N)")
-done = False
-while not done:
-    if choice.lower() == "y":
-        for email in emails:
-            print("Sending email to %s." % email.customer.data["Name"])
-            email.send()
-        done = True
-    elif choice.lower() != "n":
-        print("Invalid choice, try again.")
-    else:
-        done = True
-os.remove("../data/temp.docx")
+i = invoice.Invoice(customers[11], owner)
+# emails = []
+# for customer in customers:
+#     email = InvoiceEmail(owner, customer)
+#     emails.append(email)
+#
+#
+# print("Sending following emails: ")
+# for email in emails:
+#     print(email.customer.data["Name"])
+#
+# done = False
+# while not done:
+#     choice = input("Continue? (Y/N)")
+#     if choice.lower() == "y":
+#         for email in emails:
+#             print("Sending email to %s." % email.customer.data["Name"])
+#             email.send()
+#         done = True
+#     elif choice.lower() != "n":
+#         print("Invalid choice, try again.")
+#     else:
+#         done = True
+# os.remove("../data/temp.docx")
