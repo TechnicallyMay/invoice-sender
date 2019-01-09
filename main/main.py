8c1640b9aa6924006ef666d22af2d54498c1cc0f
TechnicallyMay <masonwells01@gmail.com>
2019-01-08 21:39:22 -0700

    Now prints data as it is generated, rather than in a seperate loop.

--- main/main.py
+++ main/main.py
@@ -1,32 +1,29 @@
 import people
 import pandas as pd
 import os
-import invoice
-import translate
 
 
 owner = people.Owner()
 customers = []
 customer_file = pd.read_excel("../data/customers.xlsx")
 num_of_customers = customer_file.shape[0]
-for i in range(num_of_customers):
-    customers.append(people.Customer(i, owner))
 
 print("Sending following emails: \n")
 any_invoices = False
-for customer in customers:
-    if customer.send_invoice:
+for i in range(num_of_customers):
+    customers.append(people.Customer(i, owner))
+    if customers[i].send_invoice:
         any_invoices = True
-        print("Invoice to %s:" % (customer.data["Name"]))
-        for charge in customer.charges:
+        print("Invoice to %s:" % (customers[i].data["Name"]))
+        for charge in customers[i].charges:
             print(charge[1], end = ": ")
             print("${:0.2f}".format(charge[3]))
-        print("Total: ${:0.2f}\n".format(customer.total))
+        print("Total: ${:0.2f}\n".format(customers[i].total))
     else:
-        print("Announcement to %s\n" % customer.data["Name"])
+        print("Announcement (no invoice) to %s\n" % customers[i].data["Name"])
 if any_invoices:
     print("Check 'temp' folder to see invoices.")
-#
+
 # done = False
 # while not done:
 #     choice = input("Continue? (Y/N)")
@@ -34,12 +31,15 @@ if any_invoices:
 #         for customer in customers:
 #             print("Sending email to %s." % customer.data["Name"])
 #             customer.email.send()
+#              if customer.send_invoice:
+#                  os.remove(customer.invoice_file_name)
 #             print("Sent")
 #         done = True
 #     elif choice.lower() != "n":
 #         print("Invalid choice, try again.")
 #     else:
 #         done = True
+#delete this when uncomment
 for customer in customers:
     if customer.send_invoice:
         os.remove(customer.invoice.file_name)
