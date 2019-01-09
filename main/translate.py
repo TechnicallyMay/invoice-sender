1c9e9bffcd2f1b41ad9c6ab5a519448223cacc71
TechnicallyMay <masonwells01@gmail.com>
2019-01-08 20:07:52 -0700

    Customer is now optional

--- main/translate.py
+++ main/translate.py
@@ -11,22 +11,27 @@ for i, month in enumerate(months):
         next_month = months[(i + 1) % len(months)]
 
 
-def translate(text, c, o):
-    translations = [("<customer.first_name>", c["First"]),
-                    ("<customer.full_name>", c["Name"]),
-                    ("<customer.number_of_students>", c["Students"]),
-                    ("<customer.monthly_rate>", c["Rate"]),
-                    ("<customer.num_of_lessons>", c["Lessons"]),
-                    ("<owner.full_name>", o["Name"]),
-                    ("<owner.cell_number>", o["Cell"]),
-                    ("<owner.address>", o["Address"]),
-                    ("<owner.city_state_zip>", o["City, State, Zip"]),
-                    ("<date>", datetime.now().strftime("%B %d, %Y")),
+def translate(text, o, c=None):
+    translations = [("<date>", datetime.now().strftime("%B %d, %Y")),
                     ("<month>", current_month),
                     ("<next_month>", next_month),
                     ("<due_date>", o["Due Date"]),
                     ("<header>", o["Header"]),
-                    ("<title>", o["Title"])]
+                    ("<title>", o["Title"]),
+                    ("<owner.full_name>", o["Name"]),
+                    ("<owner.cell_number>", o["Cell"]),
+                    ("<owner.address>", o["Address"]),
+                    ("<owner.city_state_zip>", o["City, State, Zip"])
+                    ]
+    if c:
+        cust_translations = [("<customer.first_name>", c["First"]),
+                             ("<customer.full_name>", c["Name"]),
+                             ("<customer.number_of_students>", c["Students"]),
+                             ("<customer.monthly_rate>", c["Rate"]),
+                             ("<customer.num_of_lessons>", c["Lessons"])
+                            ]
+        for trans in cust_translations:
+            translations.append(trans)
     for translation in translations:
         text = text.replace(translation[0], str(translation[1]))
     return text
