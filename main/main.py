a273e1beb9c7616af988b41970bea2919970b88a
TechnicallyMay <masonwells01@gmail.com>
2019-01-10 14:46:09 -0700

    Added record-keeping functionality

--- main/main.py
+++ main/main.py
@@ -1,7 +1,8 @@
 import people
 import pandas as pd
 import os
-
+import shutil
+from datetime import datetime
 
 owner = people.Owner()
 customers = []
@@ -14,7 +15,7 @@ for i in range(num_of_customers):
     customers.append(people.Customer(i, owner))
     if customers[i].send_invoice:
         any_invoices = True
-        print("Invoice to %s:" % (customers[i].data["Name"]))
+        print("Invoice to %s (%s):" % (customers[i].data["Name"], customers[i].data["Email"]))
         for charge in customers[i].charges:
             print(charge[1], end = ": ")
             print("${:0.2f}".format(charge[3]))
@@ -24,22 +25,28 @@ for i in range(num_of_customers):
 if any_invoices:
     print("Check 'temp' folder to see invoices.")
 
-# done = False
-# while not done:
-#     choice = input("Continue? (Y/N)")
-#     if choice.lower() == "y":
-#         for customer in customers:
-#             print("Sending email to %s." % customer.data["Name"])
-#             customer.email.send()
-#              if customer.send_invoice:
-#                  os.remove(customer.invoice_file_name)
-#             print("Sent")
-#         done = True
-#     elif choice.lower() != "n":
-#         print("Invalid choice, try again.")
-#     else:
-#         done = True
-#delete this when uncomment
-for customer in customers:
-    if customer.send_invoice:
-        os.remove(customer.invoice.file_name)
+done = False
+while not done:
+    choice = input("Continue? (Y/N): ").lower().strip()
+    if choice == "y":
+        for customer in customers:
+            print("Sending email to %s." % customer.data["Name"])
+            #customer.email.send()
+            if customer.send_invoice:
+                os.remove(customer.invoice.file_name)
+            print("Sent")
+        done = True
+    elif choice != "n":
+        print("Invalid choice, try again.")
+    else:
+        for customer in customers:
+            os.remove(customer.invoice.file_name)
+        done = True
+date = datetime.now()
+year = date.strftime("%Y")
+month = input("Name backup: ")
+year_dir = "../data/Records/" + year
+file_name = year_dir + "/" + month + ".xlsx"
+if not os.path.exists(year_dir):
+    os.makedirs(year_dir)
+shutil.copyfile("../data/customers.xlsx", file_name)
